e556d58a726ace343c911626d3c5beebf4314c7a
Leandro Ferreti <leandro.ferretti@havan.com.br>
2021-04-15 09:03:57 -0300

    Adicionar arquivos de projeto.

---
+++ TaxaJuros/Constantes/Constante.cs
@@ -0,0 +1,7 @@
+﻿namespace TaxaJuros.Constantes
+{
+    internal static class Constante
+    {
+        public const double TAXA_DE_JUROS = 0.01;
+    }
+}
---
+++ TaxaJuros/Controllers/TaxaJurosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaxaJuros.Constantes;
+
+namespace TaxaJuros.Controllers
+{
+    [ApiController]
+    public class TaxaJurosController : ControllerBase
+    {
+        public TaxaJurosController()
+        {
+        }
+
+        [HttpGet("taxaJuros")]
+        public ActionResult<double> BuscarTaxaJuros()
+        {
+            return Constante.TAXA_DE_JUROS;
+        }
+    }
+}
